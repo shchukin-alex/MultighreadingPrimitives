d1605c574378e59a400eab2b2a25466d3e58f8ce
Alex <ashchukin@paypal.com>
2023-04-13 17:47:55 +0200

    Initial Commit

---
+++ MultithreadingPrimitives/MultithreadingPrimitives.h
@@ -0,0 +1,18 @@
+//
+//  MultithreadingPrimitives.h
+//  MultithreadingPrimitives
+//
+//  Created by Alex Shchukin on 2023-04-13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultithreadingPrimitives.
+FOUNDATION_EXPORT double MultithreadingPrimitivesVersionNumber;
+
+//! Project version string for MultithreadingPrimitives.
+FOUNDATION_EXPORT const unsigned char MultithreadingPrimitivesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultithreadingPrimitives/PublicHeader.h>
+
+
